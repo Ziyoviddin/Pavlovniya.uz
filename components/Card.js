0a19086ffea19614b83a01bcd45c409aede81bb1
Ziyoviddin <ziyoviddinakramov@gmail.com>
2023-02-22 11:20:40 +0300

    card changes

--- components/Card.js
+++ components/Card.js
@@ -3,11 +3,11 @@ import Image from 'next/image'
 
 const Card = ({image,title,description,cost}) => {
   return (
-    <div className='flex flex-col justify-center items-center rounded shadow-md shadow-[#3b5572] m-3 min-[900px]:m-6'>
-        <Image src={image} alt='img' className='rounded h-[300px] w-[280px]'/>
-        <h1 className='font-bold font-sans text-[#60a24c] text-center w-[80%] py-2'>{title}</h1>
-        <p className='w-40 text-sm font-semibold'>{description}</p>
-        <h3 className='font-semibold font-sans text-teal-900 text-center py-2'><span className='font-medium text-[#7f2a4b]'>Нарх:</span>{cost}</h3>
+    <div className='flex flex-col justify-center items-center rounded shadow-md shadow-[#3b5572] m-3 min-[900px]:m-6 '>
+        <Image src={image} alt='img' className='rounded h-[300px] w-[280px] inline-block'/>
+        <h1 className='font-bold font-sans text-[#60a24c] text-center w-[80%] py-2  inline-block'>{title}</h1>
+        <p className='w-40 text-sm font-semibold inline-block h-20'>{description}</p>
+        <h3 className='font-semibold font-sans text-teal-900 text-center py-2 inline-block'><span className='font-medium text-[#7f2a4b]'>Нарх:</span>{cost}</h3>
     </div>
   )
 }
--- components/Service.js
+++ components/Service.js
@@ -7,13 +7,13 @@ import {BiSupport} from 'react-icons/bi'
 
 const Service = () => {
   return (
-    <div className='flex flex-col bg-teal-50 p-20 mt-5 '>
+    <div className='flex flex-col bg-teal-50 min-[640px]:p-20 mt-5 '>
         <div className='flex justify-center items-center pb-8  text-2xl font-bold tracking-wider'>
             <h1 className='text-[#292828]'><Link href='/service' className='hover:cursor-pointer hover:underline decoration-teal-500'>Бизнинг <span className='text-[#75d80b]'> Хизматлар
 </span></Link></h1>
         </div>
-        <div className='grid grid-cols-2 max-md:grid-cols-1 justify-center max-[600px]:flex-col'>
-            <div className='flex flex-row p-2 shadow-md shadow-[#363333] mt-4 mx-3 bg-[#f8faf9]'>
+        <div className='grid grid-cols-2 max-md:grid-cols-1 md:gap-4 justify-center max-[600px]:flex-col'>
+            <div className='flex flex-row p-2 shadow-md shadow-[#363333] mt-4 bg-[#f8faf9] mx-auto max-sm:w-[80vw]'>
                 <div className='text-5xl text-[#75d80b] mx-2'>
                     <SiOverleaf/>
                 </div>
@@ -22,7 +22,7 @@ const Service = () => {
                     <p className='italic'>Ёгоч махсулоти учун мулжалланган нихолча навларини етиштириш ва арзон нархда харидорларга йетказиш.</p>
                 </div>
             </div>
-            <div className='flex flex-row p-2 shadow-md shadow-[#363333] mt-4 mx-3  bg-[#f8faf9] '>
+            <div className='flex flex-row p-2 shadow-md shadow-[#363333] mt-4 bg-[#f8faf9] mx-auto max-sm:w-[80vw]'>
                 <div className='text-5xl text-[#75d80b] mx-2'>
                     <SiGumtree/>
                 </div>
@@ -32,8 +32,8 @@ const Service = () => {
                 </div>
             </div>
         </div>
-        <div className='grid grid-cols-2 max-md:grid-cols-1 justify-center max-[600px]:flex-col'>
-        <div className='flex flex-row p-2 shadow-md shadow-[#363333] mt-4 mx-3 bg-[#f8faf9]'>
+        <div className='grid grid-cols-2 mb-4 max-md:grid-cols-1 md:gap-4 justify-center max-[600px]:flex-col'>
+        <div className='flex flex-row p-2 shadow-md shadow-[#363333] mt-4 bg-[#f8faf9] mx-auto max-sm:w-[80vw]'>
                 <div className='text-5xl text-[#75d80b] mx-2'>
                     <TbTruckDelivery/>
                 </div>
@@ -42,7 +42,7 @@ const Service = () => {
                     <p className='italic'>Мижозларга харидларига мос келадиган доставка хизматларида ёрдам бериш. </p>
                 </div>
             </div>
-            <div className='flex flex-row p-2 shadow-md shadow-[#363333] mt-4 mx-3 bg-[#f8faf9] '>
+            <div className='flex flex-row p-2 shadow-md shadow-[#363333] mt-4 bg-[#f8faf9] mx-auto max-sm:w-[80vw]'>
                 <div className='text-5xl text-[#75d80b] mx-2'>
                     <BiSupport/>
                 </div>
